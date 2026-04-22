add3df638b207c9846dca9bf6c3a26ec6b99a227
Mau <alan.fernandez@ucb.edu.bo>
2026-04-22 16:35:12 -0400

    coments: comentarios en el tempalte de correo y sendEmail funcion

--- src/email/sendEmail.js
+++ src/email/sendEmail.js
@@ -1,9 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, html) => {
+
   const transporter = nodemailer.createTransport({
     service: "gmail",
-    
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
