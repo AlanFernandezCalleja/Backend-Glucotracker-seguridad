ca8eaa12b4ddddfbe37ec822bf7993b923f24d31
Mau <alan.fernandez@ucb.edu.bo>
2026-04-23 23:24:36 -0400

    add: Agregar a los endpoints de admin routes el uso de tokens

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,16 +3,17 @@ const router = express.Router();
 
 const {pacientesActivos, pacientesSolicitantes,activarPaciente,medicosActivos,medicosSolicitantes, activarMedico,
     perfilAdmin,agregarAdmin,obtenerAdmins}=require('../controllers/admin.controller');
-const auditoriaAdmin=require("../middlewares/auditoria.admin")
-router.post('/agregar',auditoriaAdmin,agregarAdmin);
+const auditoriaAdmin=require("../middlewares/auditoria.admin");
+const verificarToken = require('../utils/verificarToken');
+router.post('/agregar',verificarToken,auditoriaAdmin,agregarAdmin);
 
-router.get('/pacientes/activos',pacientesActivos);
-router.get('/pacientes/solicitantes',pacientesSolicitantes)
+router.get('/pacientes/activos',verificarToken, pacientesActivos);
+router.get('/pacientes/solicitantes', verificarToken,pacientesSolicitantes)
 router.get('/obtenerAdmins/:idAdmin',auditoriaAdmin,obtenerAdmins);
 router.put('/paciente/activar/:idPaciente',auditoriaAdmin,activarPaciente)
 
 
-router.get('/medicos/activos', medicosActivos);
+router.get('/medicos/activos', verificarToken,medicosActivos);
 router.get('/medicos/solicitantes',medicosSolicitantes);
 router.get('/perfilAdmin/:idUsuario',auditoriaAdmin,perfilAdmin)
 router.put('/medico/activar/:idMedico', auditoriaAdmin,activarMedico);
